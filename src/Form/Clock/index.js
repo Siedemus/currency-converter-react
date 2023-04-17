9b298a27de23f18887136bbfe70c230ae6f66137
Bartosz Sakiewa <bar11sak7@gmail.com>
2023-04-17 21:39:49 +0200

    change way of storage default date format optionsand add function which formatting date

--- src/Form/Clock/index.js
+++ src/Form/Clock/index.js
@@ -1,16 +1,23 @@
 import { useEffect, useState } from "react";
 import "./style.css";
 
+const DATE_FORMAT_OPTIONS = {
+    weekday: "long",
+    day: "2-digit",
+    month: "short",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit"
+};
+
+const formatDate = (date) => {
+    return (
+        date.toLocaleString(undefined, DATE_FORMAT_OPTIONS)
+    )
+};
+
 export const Clock = () => {
-    const options = {
-        weekday: "long",
-        day: "2-digit",
-        month: "short",
-        hour: "2-digit",
-        minute: "2-digit",
-        second: "2-digit"
-    };
-    const [date, setDate] = useState(new Date())
+    const [date, setDate] = useState(new Date());
 
     useEffect(() => {
         const intervalId = setInterval(() => {
@@ -21,6 +28,6 @@ export const Clock = () => {
     }, []);
 
     return (
-        <div className="clock">Dziś jest {date.toLocaleString(undefined, options)}</div>
-    )
+        <div className="clock">Dziś jest {formatDate(date)}</div>
+    );
 };
