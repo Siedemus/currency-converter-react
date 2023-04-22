62cffb1f592d5005c90117a955eebd2c3663bb6b
Bartosz Sakiewa <barsak1177@gmail.com>
2023-04-23 01:32:57 +0200

    add theme

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import { Header } from "./Header";
 import { Form } from "./Form";
+import { ThemeProvider } from "styled-components";
+
+const theme = {
+  colors: {
+    grey: "rgba(45, 50, 52, 0.95)",
+    tan: "rgb(223, 205, 195)",
+  }
+}
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Header />
       <Form />
-    </>
+    </ThemeProvider>
   );
-};
+}
 
 export default App;
