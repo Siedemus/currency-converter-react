6b946ee5549d47107c9691d834a1030ce0d030ca
Bartosz Sakiewa <barsak1177@gmail.com>
2023-04-23 02:31:37 +0200

    add StyledClock styled component

--- src/Form/Clock/index.js
+++ src/Form/Clock/index.js
@@ -1,33 +1,29 @@
 import { useEffect, useState } from "react";
-import "./style.css";
+import { StyledClock } from "./styled";
 
 const DATE_FORMAT_OPTIONS = {
-    weekday: "long",
-    day: "2-digit",
-    month: "short",
-    hour: "2-digit",
-    minute: "2-digit",
-    second: "2-digit"
+  weekday: "long",
+  day: "2-digit",
+  month: "short",
+  hour: "2-digit",
+  minute: "2-digit",
+  second: "2-digit",
 };
 
 const formatDate = (date) => {
-    return (
-        date.toLocaleString(undefined, DATE_FORMAT_OPTIONS)
-    )
+  return date.toLocaleString(undefined, DATE_FORMAT_OPTIONS);
 };
 
 export const Clock = () => {
-    const [date, setDate] = useState(new Date());
+  const [date, setDate] = useState(new Date());
 
-    useEffect(() => {
-        const intervalId = setInterval(() => {
-            setDate(new Date())
-        }, 1000);
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setDate(new Date());
+    }, 1000);
 
-        return () => clearInterval(intervalId)
-    }, []);
+    return () => clearInterval(intervalId);
+  }, []);
 
-    return (
-        <div className="clock">Dziś jest {formatDate(date)}</div>
-    );
+  return <StyledClock>Dziś jest {formatDate(date)}</StyledClock>;
 };
---
+++ src/Form/Clock/styled.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+const StyledClock = styled.div`
+  align-self: center;
+`;
+
+export { StyledClock };
