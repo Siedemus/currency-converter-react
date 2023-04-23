07e7850d6e61735104cc8103257cff85cb0612f3
Bartosz Sakiewa <barsak1177@gmail.com>
2023-04-23 02:39:05 +0200

    add useClock hook

--- src/Form/Clock/index.js
+++ src/Form/Clock/index.js
@@ -1,29 +1,19 @@
-import { useEffect, useState } from "react";
 import { StyledClock } from "./styled";
+import { useClock } from "./useClock";
 
 const DATE_FORMAT_OPTIONS = {
-  weekday: "long",
-  day: "2-digit",
-  month: "short",
-  hour: "2-digit",
-  minute: "2-digit",
-  second: "2-digit",
-};
-
-const formatDate = (date) => {
-  return date.toLocaleString(undefined, DATE_FORMAT_OPTIONS);
+    weekday: "long",
+    day: "2-digit",
+    month: "short",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
 };
 
 export const Clock = () => {
-  const [date, setDate] = useState(new Date());
+    const date = useClock(DATE_FORMAT_OPTIONS);
 
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      setDate(new Date());
-    }, 1000);
-
-    return () => clearInterval(intervalId);
-  }, []);
-
-  return <StyledClock>Dziś jest {formatDate(date)}</StyledClock>;
+    return (
+        <StyledClock>Dziś jest {date}</StyledClock>
+    );
 };
