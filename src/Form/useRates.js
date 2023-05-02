9553a4300073cbed52a09bbf77deaae8bb68ffa5
Bartosz Sakiewa <barsak1177@gmail.com>
2023-05-03 00:42:43 +0200

    create url

--- src/Form/useRates.js
+++ src/Form/useRates.js
@@ -2,13 +2,12 @@ import { useEffect, useState } from "react";
 
 export const useRates = () => {
   const [data, setData] = useState({ status: "waiting" });
+  const url = "https://api.exchangerate.host/latest?symbols=USD,EUR,PLN,GBP";
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch(
-          "https://api.exchangerate.host/latest?symbols=USD,EUR,PLN,GBP"
-        );
+        const response = await fetch(url);
         const jsonData = await response.json();
         setData({
           status: "success",
