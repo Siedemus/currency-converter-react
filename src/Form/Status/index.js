03a1fb4024490275146b0a34f81a56cacf104aa4
Bartosz Sakiewa <barsak1177@gmail.com>
2023-05-03 00:57:12 +0200

    delete Status component

--- src/Form/Status/index.js
+++
@@ -1,5 +0,0 @@
-import { StyledStatus } from "./styled";
-
-export const Status = ({ content }) => {
-  <StyledStatus>{content}</StyledStatus>;
-};
--- src/Form/Status/styled.js
+++
@@ -1,7 +0,0 @@
-import styled from "styled-components";
-
-export const StyledStatus = styled.p`
-  padding: 20px;
-  border: 2px dashed;
-  font-size: 15px;
-`;
