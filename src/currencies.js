f3dc97464e67df912a8f5b0f91a626589edf69b7
Bartosz Sakiewa <barsak1177@gmail.com>
2023-05-03 01:41:53 +0200

    delete currencies file

--- src/currencies.js
+++
@@ -1,6 +0,0 @@
-export const currencies = [
-    { name: "Funt Brytyjski", short: "GBP", cost: 0.190158 },
-    { name: "Dolar Amerykański", short: "USD", cost: 0.238203 },
-    { name: "Euro", short: "EUR", cost: 0.215584 },
-    { name: "Frank Szwajcarski", short: "CHF", cost: 0.211851}
-]
