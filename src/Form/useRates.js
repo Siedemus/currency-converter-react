e02fbb04202dfc8fc5d66ca88105a7b002b282b5
Bartosz Sakiewa <barsak1177@gmail.com>
2023-05-03 00:59:17 +0200

    change letter size

--- src/Form/useRates.js
+++ src/Form/useRates.js
@@ -2,12 +2,12 @@ import { useEffect, useState } from "react";
 
 export const useRates = () => {
   const [data, setData] = useState({ status: "waiting" });
-  const url = "https://api.exchangerate.host/latest?symbols=USD,EUR,PLN,GBP";
+  const URL = "https://api.exchangerate.host/latest?symbols=USD,EUR,PLN,GBP";
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch(url);
+        const response = await fetch(URL);
         const jsonData = await response.json();
         setData({
           status: "success",
