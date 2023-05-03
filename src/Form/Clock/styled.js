17c50a3073d53fa9b30ec6a7b28575896567b645
Bartosz Sakiewa <barsak1177@gmail.com>
2023-05-03 16:53:32 +0200

    add media querry to StyledClock

--- src/Form/Clock/styled.js
+++ src/Form/Clock/styled.js
@@ -2,6 +2,11 @@ import styled from "styled-components";
 
 const StyledClock = styled.div`
   align-self: center;
+  text-align: center;
+
+  @media (max-width: 992px) {
+    margin-top: 20px;
+  }
 `;
 
 export { StyledClock };
